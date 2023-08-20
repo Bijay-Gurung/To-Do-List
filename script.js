bdd1a0481afbbce590d6eb4988a100052e42f68e
Bijay-Gurung <stha4580@gmail.com>
2023-08-20 15:43:07 +0545

    adding some js and updating html and css

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Still in developing Phase!");
+let input = document.getElementById('input');
+let display = document.getElementById('display');
+let mark = document.getElementById('mark');
+
+function add(){
+    display.textContent = input.value;
+}
+function check(){
+    display.style.textDecoration = 'line-through';
+}
+function remove(){
+    display.remove();
+}
