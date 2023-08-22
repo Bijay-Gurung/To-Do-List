1374e35e98fb60304f346c05c8b9039b12c6ded1
Bijay-Gurung <stha4580@gmail.com>
2023-08-22 10:15:45 +0545

    adding Local storage

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 let input = document.getElementById('input');
 let Lists = document.getElementById('node');
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-function add(){
+// Function to populate the task list from the tasks array
+function populateTaskList() {
+    Lists.innerHTML = ''; // Clear the existing list
+    tasks.forEach(taskText => {
+        let newList = document.createElement('li');
+
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.addEventListener('change', check);
+
+        let task = document.createElement('p');
+        task.textContent = taskText;
+
+        let removebtn = document.createElement('button');
+        removebtn.onclick = remove;
+        removebtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+
+        newList.appendChild(checkbox);
+        newList.appendChild(task);
+        newList.appendChild(removebtn);
+
+        Lists.appendChild(newList);
+    });
+}
+
+// Call the function to populate the task list on page load
+populateTaskList();
+
+function add() {
+    let taskText = input.value;
+    if (taskText.trim() === '') {
+        return; // Don't add empty tasks
+    }
+
     let newList = document.createElement('li');
 
     let checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
-    checkbox.onclick = check;
+    checkbox.addEventListener('change', check);
 
     let task = document.createElement('p');
-    task.textContent = input.value;
+    task.textContent = taskText;
 
     let removebtn = document.createElement('button');
     removebtn.onclick = remove;
@@ -20,19 +54,29 @@ function add(){
     newList.appendChild(removebtn);
 
     Lists.appendChild(newList);
+
+    tasks.push(taskText);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
     input.value = '';
 }
 
-function check(){
+function check(event) {
     let task = event.target.nextSibling;
-    if(event.target.checked){
+    if (event.target.checked) {
         task.style.textDecoration = 'line-through';
-    }else{
+    } else {
         task.style.textDecoration = 'none';
     }
 }
 
-function remove(){
+function remove() {
     let listItem = this.parentNode;
+    let taskText = listItem.querySelector('p').textContent;
+    let taskIndex = tasks.indexOf(taskText);
+    if (taskIndex !== -1) {
+        tasks.splice(taskIndex, 1);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
     listItem.parentNode.removeChild(listItem);
 }
